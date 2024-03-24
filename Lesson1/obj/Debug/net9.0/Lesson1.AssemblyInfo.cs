0c4ed7ce888b5d8dda9387bd7b7a7c39e70c2359
Lukin Lukina <a.lukin@rambler.ru>
2024-03-24 18:16:59 +0300

    Добавили несколько переменных

--- Lesson1/obj/Debug/net9.0/Lesson1.AssemblyInfo.cs
+++ Lesson1/obj/Debug/net9.0/Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bea512b318c8ad6b695e71abf0cee6b3fa4d66e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
