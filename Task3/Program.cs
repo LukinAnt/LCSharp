9deb359eba7831ee19bced80d6d1c4928bb42bd8
Lukin Lukina <a.lukin@rambler.ru>
2024-03-24 20:19:01 +0300

    сравнение А и Б

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//мы сравниваем А с Б и если А то выводим А, а если нет то выводим Б
+
+int a = 5;
+int b = 7;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+
+}
+else
+{
+    Console.WriteLine(b);
+}
