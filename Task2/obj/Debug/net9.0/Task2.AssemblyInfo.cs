a9055ee52c1f674a0fc6a7a30c7209524339269f
Lukin Lukina <a.lukin@rambler.ru>
2024-03-24 19:43:21 +0300

    добавили деление

--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4ed7ce888b5d8dda9387bd7b7a7c39e70c2359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3151c3fc607971c161c13ec2a71c289e3cff0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
