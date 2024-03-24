8cb49dff6788d441a079f4e040b4c83c295049cb
Lukin Lukina <a.lukin@rambler.ru>
2024-03-24 20:04:30 +0300

    дз из лекции я сам

---
+++ Task22/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 100;
+int b = 6;
+int c = 4;
+int d = 5;
+int sum = a * b;
+Console.WriteLine(sum);
+
+double product1 = c + d;
+Console.WriteLine(product1);
+
+
+double quotient2 = sum / product1;
+Console.WriteLine(quotient2);
