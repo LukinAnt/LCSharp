48f6b21c8257004d244e19636ab226661fd8ef40
Lukin Lukina <a.lukin@rambler.ru>
2024-03-28 22:15:05 +0300

    Lesson3

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -57,7 +57,7 @@ while (i < arr.Length)
     i = i + 1;
 }
 */
-
+/*
 int n = 5;
 int[] arr = { 2, 4, 7, 3, 9 };
 int i = 0;
@@ -91,3 +91,64 @@ foreach (int e in arr)
     }
 }
 Console.WriteLine(max);
+*/
+
+//Лекция 3
+/*
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result);
+*/
+/*
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
+*/
+
+
+
+/*
+void ZeroEventElements(int[] arr)
+//обнулить четные элементы
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEventElements(array);
+PrintArray(array);
+*/
+
+
+
+
+
+
